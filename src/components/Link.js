c8dbdd6038bc9a4b321a839cf7656d379292f213
Anderson Lima <andersonlimahw@gmail.com>
2018-04-22 20:21:05 -0300

    create empty files

---
+++ src/components/Link.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Todo = ({}) => (
+    <li> </li>
+)
+
+Todo.propTypes = {
+    text: PropTypes.string.isRequired
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
+
+import {render } from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import rootReducer from './reducers';
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
